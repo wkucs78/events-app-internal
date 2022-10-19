ead75018e0ecc0ac131333df33bb5f022ae492a4
wkucs78 <wkucs78@gmail.com>
2022-10-19 15:27:30 +0000

    Initial commit

---
+++ load.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS events_db;
+CREATE DATABASE events_db;
+
+USE events_db;
+
+CREATE TABLE events(
+   id INT NOT NULL AUTO_INCREMENT,
+   title VARCHAR(255) NOT NULL,
+   description TEXT NOT NULL,
+   location VARCHAR(255) NOT NULL,
+   image VARCHAR(255) NOT NULL,
+   likes INT DEFAULT 0,
+   datetime_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY ( id )
+);
+
+SHOW TABLES;
+
+INSERT INTO events (title, description, location, image) VALUES ('Pet Show', 'Super-fun with furry friends!', 'Dog Park', '');
+
+INSERT INTO events (title,  description, location, image) VALUES ('Company Picnic', 'Come for free food and drinks.', 'At the lake', '');
+
+
+SELECT * FROM events;
